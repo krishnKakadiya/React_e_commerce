435655a55fc1476ce098f6f75b7aab514c7b1787
krishnKakadiya <kakadiyakrishn@gmail.com>
2023-06-14 17:58:27 +0530

    add category in home page

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import "./Category.scss";
+import Cat1 from "../../assetes/iphone.jpg"
 
-const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+const Category = ({ categories }) => {
+    const navigate = useNavigate();
+    return (
+        <div className="shop-by-category">
+            <div className="categories">
+               <div className="category">
+                <img src={Cat1} alt="" />
+               </div>
+               <div className="category">
+                <img src={Cat1} alt="" />
+               </div>
+               <div className="category">
+                <img src={Cat1} alt="" />
+               </div>
+               <div className="category">
+                <img src={Cat1} alt="" />
+               </div>
+            </div>
+        </div>
+    );
+};
 
-export default Category
+export default Category;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Home.scss';
 import Banner from './Banner/Banner';
+import Category from '../Category/Category';
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div>
       <Banner />
+    <div className="main-contenet">
+       <div className="layout">
+        <Category />
+       </div>
+    </div>
     </div>
   )
 }
