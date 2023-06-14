ea791950beadda4b6c6601a9969a08651feef269
krishnKakadiya <kakadiyakrishn@gmail.com>
2023-06-14 17:29:52 +0530

    add footersection

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Category from "./components/Category/Category";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
 // import AppContext from "./Utils/Context";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
               <Route path="/category/:id" element={<Category />} />
               <Route path="/product/:id" element={<SingleProduct />} />
             </Routes>
+            <Newsletter />
         <Footer />
       {/* </AppContext> */}
     </Router>
