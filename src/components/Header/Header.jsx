6ad902146264ae8b9e5b48c87fae7a483d1ddba5
krishnKakadiya <kakadiyakrishn@gmail.com>
2023-06-16 18:59:16 +0530

    add Search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,13 @@ import { TbSearch } from "react-icons/tb";
 import { AiOutlineHeart } from "react-icons/ai";
 import { CgShoppingCart } from "react-icons/cg";
 import Cart from "../Cart/Cart";
+import Search from "./Search/Search";
 
 const Header = () => {
 
   const [scrolled, setScrolled] = useState(false);
   const [showCart, setShowCart] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
 
 const handleScroll = () => {
   const offset = window.scrollY;
@@ -39,7 +41,7 @@ const handleScroll = () => {
         </ul>
         <div className="center">KNOXStore</div>
         <div className="right">
-          <TbSearch />
+          <TbSearch onClick={()=> setShowSearch(true)}/>
           <AiOutlineHeart />
           <span className="cart-icon" onClick={() => setShowCart(true)}>
             <CgShoppingCart />
@@ -49,6 +51,7 @@ const handleScroll = () => {
       </div>
     </header>
     {showCart && <Cart setShowCart={setShowCart} />}
+    {showSearch && <Search setShowSearch={setShowSearch} />}
     </>
   );
 };
--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./Search.scss";
+import { MdClose } from "react-icons/md";
+import Result from "../../../assetes/product.jpg";
 
-const Search = () => {
+const Search = ({ setShowSearch }) => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search-modal">
+      <div className="form-field">
+        <input type="text" autoFocus placeholder="serch for products" />
 
-export default Search
+        <MdClose className="close-btn" onClick={() => setShowSearch(false)} />
+      </div>
+      <div className="search-result-content">
+        <div className="search-results">
+          <div className="search-result-item">
+            <div className="img-container">
+              <img src={Result} alt="" />
+            </div>
+            <div className="prod-details">
+              <span className="name">
+                Product name
+              </span>
+              <span className="desc">
+                description
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
